ab80efa8b054ce1e51daa1ee56db93f9c4727cf4
Amir Lajevardi <amir.lajevardi@city.ac.uk>
2025-03-25 01:01:28 +0000

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Amir Lajevardi on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
