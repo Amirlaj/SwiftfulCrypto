68e8e5fe3b4b681b22eea8733adf33452eb99c10
Amir Lajevardi <amir.lajevardi@city.ac.uk>
2025-03-25 15:31:03 +0000

    session 1 again

--- SwiftfulCrypto/Extensions/Color.swift
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccencetColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("redColor")
+    let secondaryText = Color("SecondaryTextColor")
+
+}
